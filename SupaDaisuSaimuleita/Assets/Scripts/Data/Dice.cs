78e9394721896058f0a47b2df5e663b58e717f5d
AntoineIturbide <antoine.iturbide@gmail.com>
2016-10-21 11:16:15 +0200

    DiceData & GameManager V1

---
+++ SupaDaisuSaimuleita/Assets/Scripts/Data/Dice.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameData {
+
+    [CreateAssetMenu(fileName = "unamedDice", menuName = "Data/Dice", order = 1)]
+    public class Dice : ScriptableObject {
+
+        public DiceColor color = DiceColor.RED;
+        public byte faceCount = 1;
+        
+    }
+
+    public enum DiceColor {
+        RED,
+        BLUE,
+        GREEN
+    }
+}
